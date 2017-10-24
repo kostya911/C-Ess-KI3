36b2c916dac46bf0ee6fd627b73fa0ed66db402e
kostya911 <32157231+kostya911@users.noreply.github.com>
2017-10-24 20:44:31 +0300

    Add files via upload

---
+++ Chapter-1/Lab1/Lab1/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+	cout << "Hello";
+	cout << endl;
+	cout << "Hello";
+	cout << "\nIt's me, your first program.\n";
+
+	system("pause");
+	
+	return 0;
+}
