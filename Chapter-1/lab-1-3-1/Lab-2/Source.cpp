e3ad89740f049c708b1cbc192c2e78d5232f8332
kostya911 <32157231+kostya911@users.noreply.github.com>
2017-10-26 18:59:13 +0300

    Add files via upload

---
+++ Chapter-1/lab-1-3-1/Lab-2/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int SecondInThreeHours=10800;
+	cout << "result " << SecondInThreeHours <<endl;
+	int SecondInTreeMinute =3*60;
+	int SecondInFiveMinute=5*60;
+	double Pi=3.141526;
+	system("pause");
+	return 0;
+
+}
