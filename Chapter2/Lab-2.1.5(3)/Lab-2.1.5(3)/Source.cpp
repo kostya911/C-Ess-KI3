7a5c1683d572fdf36225d254c60b5977386557f5
kostya911 <32157231+kostya911@users.noreply.github.com>
2017-11-11 14:24:53 +0200

    Add files via upload

---
+++ Chapter2/Lab-2.1.5(3)/Lab-2.1.5(3)/Source.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iomanip>
+
+
+using namespace std;
+
+int main(void) {
+	int sys;
+	float m, ft, in;
+	do {
+		cout << "System of input data(0 - metric, 1 - imperal): ";
+		cin >> sys;
+	} while (sys < 0 || sys > 1);
+
+	if (sys == 0) {
+		cout << "Enter meters: ";
+		cin >> m;
+
+		in = m / 2.54E-2f; // only in inches
+		ft = (int)in / 12; // calculate foots
+		in = in - ft * 12; // calculate inches
+
+		cout << ft << "'" << fixed << setprecision(5) << in << "''" << endl;
+	}
+	else if (sys == 1) {
+		cout << "Enter feet: ";
+		cin >> ft;
+
+		cout << "Enter inches: ";
+		cin >> in;
+
+		m = (ft * 12 + in) * 2.54E-2f;
+		cout << m << "m" << endl;
+	}
+	return 0;
+}
