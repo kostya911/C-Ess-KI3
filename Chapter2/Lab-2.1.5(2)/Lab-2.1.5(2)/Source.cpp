98a65f56a384139088fe169b67a16a75ccfccc8a
kostya911 <32157231+kostya911@users.noreply.github.com>
2017-11-11 14:23:48 +0200

    Add files via upload

---
+++ Chapter2/Lab-2.1.5(2)/Lab-2.1.5(2)/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+int main(void) {
+	float grossprice, taxrate, netprice, taxvalue;
+
+	cout << "Enter a gross price: ";
+	cin >> grossprice;
+		do
+		{
+	cout << "Enter a tax rate: ";
+	cin >> taxrate;
+		} while (taxrate < 0 || taxrate > 100);
+
+	netprice = grossprice / (1 + taxrate / 100);
+	taxvalue = grossprice - netprice;
+
+
+	cout << "Net price: " << netprice << endl;
+	cout << "Tax value: " << taxvalue << endl;
+
+return 0;
+}
