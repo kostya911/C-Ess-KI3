1747f60556943425de99ba75be7f35820a7171cb
kostya911 <32157231+kostya911@users.noreply.github.com>
2017-11-12 13:43:18 +0200

    Add files via upload

---
+++ Chapter2/Lab-2.1.5(5)/Lab-2.1.5(5)/Lab-2.1.5(5).cpp
@@ -0,0 +1,31 @@
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int year;
+	int a, b, c, d, e;
+
+	do
+	{
+		cout << "Enter year (1900-2100): ";
+		cin >> year;
+	} while (year < 1900 || year > 2100);
+
+	a = year % 19;
+	b = year % 4;
+	c = year % 7;
+	d = (a * 19 + 24) % 30;
+	e = (2 * b + 4 * c + 6 * d + 5) % 7;
+
+	if (d + e < 10) {
+		cout << "Easter falls on the " << (d + e + 22) << " day of March." << endl;
+	}
+	else {
+		cout << "Easter falls on the " << (d + e - 9) << " day of April." << endl;
+	}
+
+	return 0;
+}
