5d9c173e39add386d0c2d35e954c48c24d9ec9be
kostya911 <32157231+kostya911@users.noreply.github.com>
2017-11-18 14:53:57 +0200

    Add files via upload

---
+++ Chapter2/Lab-2.3.19(9)/Lab-2.3.19(9)/Lab-2.3.19(9).cpp
@@ -0,0 +1,22 @@
+//The riddle
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	cout << "Enter size of the side (only odd numbers >1)\n";
+	do{
+		cin >> a;
+		if(a < 3 && a%2 != 0)
+			cout << "oops\nTry again\n";
+		else
+			cout << "Ok, solving the riidle\n";
+	}while(a < 3 && a%2 != 0);
+	//formula (2a-1)+((a-2)^2-1)
+	cout <<"Bottom right corner of square with side " <<a<<" is: " <<2*a + (a-2)*(a-2) -2<<endl;
+	system("pause");
+	return 0 ;
+}
