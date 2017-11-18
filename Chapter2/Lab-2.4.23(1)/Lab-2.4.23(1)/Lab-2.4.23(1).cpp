2611ae85040757ec2b6c9cb9fa62ea4848a6e152
kostya911 <32157231+kostya911@users.noreply.github.com>
2017-11-18 15:05:47 +0200

    Add files via upload

---
+++ Chapter2/Lab-2.4.23(1)/Lab-2.4.23(1)/Lab-2.4.23(1).cpp
@@ -0,0 +1,26 @@
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	unsigned long num;
+	int count = 0;
+	cout <<"Enter positive number\n";
+	cin >> num;
+	for(int i = 0; i < 32; i++)
+	{
+		if((num&1) == 1)
+		{
+			count++;
+			num = num >> 1;
+		}
+		else
+		num = num >> 1;
+	}
+	cout <<"Numer of 1 is: "<<count<<endl;
+	system("pause");
+	return 0;
+}
