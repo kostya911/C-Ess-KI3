6c59b003b0433beadd18947ed0b4aa4b7c9df9d0
kostya911 <32157231+kostya911@users.noreply.github.com>
2017-11-18 14:43:09 +0200

    Add files via upload

---
+++ Chapter2/Lab-2.3.19(8)/Lab-2.3.19(8)/Lab-2.3.19(8).cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	unsigned short n;
+	unsigned long res = 1;
+	cout << "Enter n to find factorial (non-negative)\n";
+	do{
+		cin >> n;
+	}while(n<0);
+
+	while(n>=1)
+	{
+		res*=n;
+		n--;
+	}
+	cout<<"factorial: "<< res <<endl;
+
+	system("pause");
+	return 0;
+}
+
