4b766d595fb52ead309d0dec9a891f422c3244e7
kostya911 <32157231+kostya911@users.noreply.github.com>
2017-11-27 20:08:08 +0200

    Add files via upload

---
+++ Chapter-3/Lab-3.6.3(1)/Lab-3.6.3(1)/Lab-3.6.3(1).cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+void increment(int &var, int add = 1)
+{
+	var+=add;
+}
+
+int main(void) {
+	int var = 0;
+	for(int i = 0; i < 10; i++)
+		if(i % 2)
+			increment(var);
+		else
+			increment(var,i);
+	cout << var << endl;
+	system("pause");
+	return 0;
+}
