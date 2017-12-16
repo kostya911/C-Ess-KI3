6f29cd90ed7e6cb6a3555638fd02f70c7976a6fc
kostya911 <32157231+kostya911@users.noreply.github.com>
2017-12-16 13:37:09 +0200

    Add files via upload

---
+++ Chapter-4/Lab-4.5.2(1) duplicate white space/Lab-4.5.2(1)/Lab-4.5.2(1).cpp
@@ -0,0 +1,25 @@
+//text manipilating remobe duplicate white spaces
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string text;
+
+	cout << "Enter text\n";
+	getline(cin, text);
+	int hasDuplicate = text.find("  ");
+
+	while (hasDuplicate != string::npos)
+	{
+		text.replace(hasDuplicate, 2, " ");
+		hasDuplicate = text.find("  ");
+	}
+	cout << text << endl;
+
+	system("pause");
+	return 0;
+}
---
+++ Chapter-4/Lab-4.5.6(1)_eliminate_dublicates/Lab-4.5.6(1)_eliminate_dublicates/Lab-4.5.6(1)_eliminate_dublicates.cpp
@@ -0,0 +1,50 @@
+//Remove duplicates
+
+#include "stdafx.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+string eraseDuplicate(string sentence)
+{
+	string str, res = "";
+	bool isReplaced;
+	int check = sentence.find(" ");
+	while(check != string::npos)
+	{
+		str = sentence.substr(0,check);//take first word
+		res+=str+" ";
+		sentence = sentence.substr(check+1,string::npos);//remove first word from sentence
+		do{
+			isReplaced = false;
+			if(sentence.find(str)!=string::npos)//check if word has duplicate
+			{
+				sentence.replace(sentence.find(str),str.length()+1,"");//remove duplicate
+				isReplaced = true;
+			}
+		}while(isReplaced);
+		check = sentence.find(" ");//where next word finishes
+	}
+	return res+sentence;//+sentence/ to capture the last word/ if i dont forget i will corect it
+}
+int main()
+{
+	string text ;
+	getline(cin, text);
+	string temp, res = "";
+
+	int check = text.find(",");
+	while(check!=string::npos)//divide sentence by commas and there for duplicate separetly
+	{
+		temp = text.substr(0,check);
+		res+=eraseDuplicate(temp)+", ";
+		text = text.substr(check+2,string::npos);//+2 to not capture comma and space ", "
+		check = text.find(",");
+	}
+	res+=eraseDuplicate(text);//if there is no commas/and cycle up there dont procces substring after last comma:)
+	
+	cout<<"RESULTS:"<<res<<":\n";
+	system("pause");
+	return 0;
+}
