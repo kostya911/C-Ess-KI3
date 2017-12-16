790877bfbc1fdc86e556d94871c77608b9b5ddb3
kostya911 <32157231+kostya911@users.noreply.github.com>
2017-12-16 12:46:42 +0200

    Add files via upload

---
+++ Chapter-4/Lab-4.2.5(1)/Lab-4.2.5(1)/Lab-4.2.5(1).cpp
@@ -0,0 +1,15 @@
+//ariphmetic progression
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	long long int N = 0;
+	cout<<"Enter N\n";
+	cin >> N;
+	N = (N+1)*N/2;
+	cout <<N<<endl;
+	system("pause");
+	return 0;
+}
