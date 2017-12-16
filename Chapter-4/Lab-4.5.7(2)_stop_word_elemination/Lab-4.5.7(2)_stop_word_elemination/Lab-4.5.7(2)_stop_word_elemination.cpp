d83285e65e0cff6abf2ffcbde94dc996b49de005
kostya911 <32157231+kostya911@users.noreply.github.com>
2017-12-16 13:40:53 +0200

    Add files via upload

---
+++ Chapter-4/Lab-4.5.7(2)_stop_word_elemination/Lab-4.5.7(2)_stop_word_elemination/Lab-4.5.7(2)_stop_word_elemination.cpp
@@ -0,0 +1,50 @@
+#include "stdafx.h"
+#include <string>
+#include <iostream>
+using namespace std;
+string toUppper(string str)
+{
+	str[0]-=32;
+	return str;
+}
+void erase(string &sentence, string str)
+{
+	bool isReplaced;
+	string upStr = toUppper(str);
+	do{
+		isReplaced = false;
+		if(sentence.find(str)!=string::npos)
+		{
+			sentence.replace(sentence.find(str),str.length()+1,"");
+			isReplaced = true;
+		}
+		else if(sentence.find(upStr)!=string::npos)
+		{
+			sentence.replace(sentence.find(upStr),upStr.length()+1,"");
+			isReplaced = true;
+		}
+	}while(isReplaced);
+
+}
+int main()
+{
+	std::string stop_words ;
+	std::getline(std::cin, stop_words);
+	std::string sentence ;
+	std::getline(std::cin, sentence);
+
+	string sw;
+	int check = 0;
+	do
+	{
+		check = stop_words.find(",");
+		sw = stop_words.substr(0,stop_words.find(","));
+		erase(sentence,sw);
+		stop_words = stop_words.substr(stop_words.find(",")+1);//dont need the word we have used/+1 to not capture comma
+	}while(check != string::npos);
+
+	cout << sentence<<endl;
+
+	system("pause");
+	return 0;
+}
