89dc007fa62bea58422d0eb7ff457323af39e88b
kostya911 <32157231+kostya911@users.noreply.github.com>
2017-12-18 22:15:42 +0200

    Add files via upload

---
+++ Chapter-5/Lab-5.3.13(1)_singly_linked_list_p1/Lab-5.3.13(1)_singly_linked_list_p1/stdafx.cpp
@@ -0,0 +1,8 @@
+// stdafx.cpp : source file that includes just the standard includes
+// Lab-5.3.13(1)_singly_linked_list_p1.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
