2b097eda674ca4f9d0c37044e5a40bf7c6a22014
kostya911 <32157231+kostya911@users.noreply.github.com>
2017-12-18 21:22:42 +0200

    Add files via upload

---
+++ Chapter-5/Lab-5.1.9(1)_classes_and_objects_in_c++/Lab-5.1.9(1)_classes_and_objects_in_c++/stdafx.cpp
@@ -0,0 +1,8 @@
+// stdafx.cpp : source file that includes just the standard includes
+// Lab-5.1.9(1)_classes_and_objects_in_c++.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
