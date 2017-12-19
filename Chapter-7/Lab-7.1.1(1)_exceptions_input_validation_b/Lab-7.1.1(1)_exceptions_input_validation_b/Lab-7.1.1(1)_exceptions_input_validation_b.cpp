3800c28690960f3c6529ea2a9dbe0a8d3a91587b
kostya911 <32157231+kostya911@users.noreply.github.com>
2017-12-20 00:08:39 +0200

    Add files via upload

---
+++ Chapter-7/Lab-7.1.1(1)_exceptions_input_validation_b/Lab-7.1.1(1)_exceptions_input_validation_b/Lab-7.1.1(1)_exceptions_input_validation_b.cpp
@@ -0,0 +1,24 @@
+// Lab-7.1.1(1)_exceptions_input_validation_b.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<iostream>
+#include <string>
+
+using namespace std;
+int main(void)
+{
+	int a = 8, b = 0, c = 0;
+	cin >> b;
+	try {
+		if (b == 0)
+			throw string("You can`t divide by zero!\n");
+		c = a / b;
+	}
+	catch (string &problem)
+	{
+		cout << problem;
+	}
+	cout << c << endl;
+	return 0;
+}
---
+++ Chapter-7/Lab-7.1.3(1)_exceptions_divide_by_zero/Lab-7.1.3(1)_exceptions_divide_by_zero/Lab-7.1.3(1)_exceptions_divide_by_zero.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include<iostream>
+#include<string>
+using namespace std;
+int main(void)
+{
+	int a = 0, b = 0, c = 0;
+	cin >> b;
+	cin >> a;
+	try
+	{
+		if (b == 0)
+			throw string("You can`t divide by zero!");
+		c = a / b;
+	}
+	catch (string &problem)
+	{
+		cout << problem << endl;
+	}
+	cout << c << endl;
+	return 0;
+}
