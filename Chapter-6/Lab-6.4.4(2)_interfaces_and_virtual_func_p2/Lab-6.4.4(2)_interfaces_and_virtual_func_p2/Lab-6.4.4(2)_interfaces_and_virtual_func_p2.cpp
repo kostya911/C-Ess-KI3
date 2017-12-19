046eb63f2618acbaea13b9239a8e74449d9c32c0
kostya911 <32157231+kostya911@users.noreply.github.com>
2017-12-20 00:03:00 +0200

    Add files via upload

---
+++ Chapter-6/Lab-6.4.4(2)_interfaces_and_virtual_func_p2/Lab-6.4.4(2)_interfaces_and_virtual_func_p2/Lab-6.4.4(2)_interfaces_and_virtual_func_p2.cpp
@@ -0,0 +1,11 @@
+// Lab-6.4.4(2)_interfaces_and_virtual_func_p2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
